f7e3ba4b7714cbb3c676dd3b2f9f5c13c9e87d4a
robert <robert.ljx.de.simon@gmail.com>
2023-01-28 19:11:47 +1100

    ModelAndView

--- src/main/java/org/example/controller/TestScopeController.java
+++ src/main/java/org/example/controller/TestScopeController.java
@@ -1,7 +1,23 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestScopeController {
+    @RequestMapping("/test/mav")
+    public ModelAndView testModelAndView(){
+        /**
+         * ModelAndView include Model and View
+         * Model:share data to request scope
+         * View: set logic view to implement page jumping
+         */
+        ModelAndView modelAndView=new ModelAndView();
+        //model
+        modelAndView.addObject("testRequestScope","hello, model and view");
+        //view
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
 }
