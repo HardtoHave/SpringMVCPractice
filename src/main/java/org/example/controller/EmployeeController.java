5a8e9cd92b139a54e4ca27534814dff081ed7082
robert <robert.ljx.de.simon@gmail.com>
2023-02-02 15:44:44 +1100

    restful

--- src/main/java/org/example/controller/EmployeeController.java
+++ src/main/java/org/example/controller/EmployeeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -18,6 +20,22 @@ public class EmployeeController {
     public String getAllEmployees(Model model){
         Collection<Employee> all = employeeDAO.getAll();
         model.addAttribute("all",all);
-        return "employee_list ";
+        return "employee_list";
+    }
+    @PostMapping("employee")
+    public String addEmployee(Employee employee){
+        employeeDAO.save(employee);
+        return "redirect:/employee";
+    }
+    @GetMapping("/employee/{id}")
+    public String update(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDAO.get(id);
+        model.addAttribute("employee", employee);
+        return "employee_update";
+    }
+    @PutMapping("employee")
+    public String updateEmployee(Employee employee){
+        employeeDAO.save(employee);
+        return "redirect:/employee";
     }
 }
